9f4eb8ee560ff17fd1299d261532bdfd8678c23e
thoriqilhamizaidan <101938819+thoriqilhamizaidan@users.noreply.github.com>
2022-04-01 23:53:32 +0700

    module 3 session 2

--- src/components/song/index.js
+++ src/components/song/index.js
@@ -1,18 +1,19 @@
 import React from "react";
 
-function Song(props){
-  return (
-
-    <div className="row">
-      <div className="card-song">
-          <img src={props.image} alt="true"/>
-          <h1>{props.title}</h1>
-          <h2>Album : {props.album}</h2>
-          <h2>Artist : {props.artist}</h2>
-          <button class="btn select">Select</button>
+const Song = (props) => {
+    return (
+  
+      <div className="row">
+        <div className="card-song">
+            <img src={props.image} alt="true"/>
+            <h1>{props.title}</h1>
+            <h2>Album : {props.album}</h2>
+            <h2>Artist : {props.artist}</h2>
+            <button class="btn select">Select</button>
+        </div>
       </div>
-    </div>
-    
-  );
+      
+    );
   }
+
 export default Song;
